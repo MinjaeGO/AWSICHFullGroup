342b366f20a6795ba21d6c55e8960a8fc6a68da6
EZEN-142 <EZEN-142@DESKTOP-88NAHRT>
2022-07-13 15:58:20 +0900

    학원 동기생 정보

---
+++ GMJ/src/gmj.java
@@ -0,0 +1,31 @@
+
+public class gmj {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("OGH " + "KSJ " + "KJS ");
+		System.out.println("KTH " + "NGH " + "KDH ");
+		System.out.println("CEJ " + "IYB " + "IJH ");
+		System.out.println("PYM " + "SDH " + "ABK ");
+		System.out.println("CYS " + "CHS " + "HWJ ");
+		
+//		String name1 = "OGH";
+//		String name2 = "KSJ";
+//		String name3 = "KJS";
+//		String name4 = "KTH"
+//		String name5 = 
+//		String name6 =
+//		String name7 =
+//		String name8 =
+//		String name9 =
+		
+		
+//		System.out.println(name1);
+				
+				
+		
+		
+		
+	}
+}
